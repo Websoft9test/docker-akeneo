14b6a9246c41c04ba2f180087145196510e01390
chendelin1982 <27513732@qq.com>
2022-02-08 16:13:20 +0800

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,6 @@ ENV AKENEO_MYSQL_PORT=3306
 ENV AKENEO_MYSQL_USER=akeneo
 ENV AKENEO_MYSQL_PASSWORD=akeneo
 ENV AKENEO_MYSQL_DATABASE=akeneo
-ENV AKENEO_SITENAME=DiscuzQ
 
 
 RUN apt update -y && apt install apache2 unzip make -y
@@ -23,16 +22,15 @@ ADD entrypoint.sh /entrypoint.sh
 RUN chmod +x entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
 
+RUN curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
+RUN bash nodesource_setup.sh && apt install nodejs
 RUN wget https://getcomposer.org/composer-stable.phar -O composer.phar; chmod 750 composer.phar;  mv composer.phar /usr/local/bin/composer
-RUN COMPOSER_MEMORY_LIMIT=-1 composer create-project akeneo/pim-community-standard /var/www/html/pim "5.0.*@stable"
+RUN rm -rf /var/www/html/*
+RUN COMPOSER_MEMORY_LIMIT=-1 composer create-project akeneo/pim-community-standard /var/www/html "5.0.*@stable"
 
 WORKDIR /var/www/html
-RUN chown -R www-data:www-data /var/www
+RUN chown -R www-data:www-data /var/www/html
 
 EXPOSE 80
 
 CMD ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
-
-NO_DOCKER=true make prod
-bin/console pim:install --force --symlink --clean --env=prod
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd /var/www/html && NO_DOCKER=true make prod
+
 # replace Akeneo env
 cp /var/www/html/pim/.env  /var/www/html/pim/.env.local
-sed -i "s/APP_DATABASE_HOST=*/APP_DATABASE_HOST=$DISCUZQ_MYSQL_HOST/"  /var/www/html/pim/.env.local
-sed -i "s/APP_DATABASE_PORT=*/APP_DATABASE_PORT=$DISCUZQ_MYSQL_PORT/"  /var/www/html/pim/.env.local
-sed -i "s/APP_DATABASE_NAME=*/AKENEO_MYSQL_DATABASE=$DISCUZQ_MYSQL_HOST/"  /var/www/html/pim/.env.local
-sed -i "s/APP_DATABASE_USER=*/APP_DATABASE_USER=$ENV AKENEO_MYSQL_USER/"  /var/www/html/pim/.env.local
-sed -i "s/APP_DATABASE_PASSWORD=*/APP_DATABASE_PASSWORD=$AKENEO_MYSQL_PASSWORD/"  /var/www/html/pim/.env.local
+sed -i "s/APP_DATABASE_HOST=*/APP_DATABASE_HOST=$DISCUZQ_MYSQL_HOST/"  /var/www/html/.env.local
+sed -i "s/APP_DATABASE_PORT=*/APP_DATABASE_PORT=$DISCUZQ_MYSQL_PORT/"  /var/www/html/.env.local
+sed -i "s/APP_DATABASE_NAME=*/AKENEO_MYSQL_DATABASE=$DISCUZQ_MYSQL_HOST/"  /var/www/html/.env.local
+sed -i "s/APP_DATABASE_USER=*/APP_DATABASE_USER=$ENV AKENEO_MYSQL_USER/"  /var/www/html/.env.local
+sed -i "s/APP_DATABASE_PASSWORD=*/APP_DATABASE_PASSWORD=$AKENEO_MYSQL_PASSWORD/"  /var/www/html/.env.local
 
 # replace php.ini and fpm vars
 sed -i "s/memory_limit = 128M/memory_limit = 512M/"  /etc/php/7.4/fpm/php.ini
