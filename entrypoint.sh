b10cf6d0bcc116eae061652b9949c93c864e4bea
chendelin1982 <27513732@qq.com>
2022-02-09 09:39:57 +0800

    sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ sed -i "s/APP_DATABASE_USER=.*/APP_DATABASE_USER=$AKENEO_MYSQL_USER/g"  /var/www
 sed -i "s/APP_DATABASE_PASSWORD=.*/APP_DATABASE_PASSWORD=$AKENEO_MYSQL_PASSWORD/g"  /var/www/html/.env.local
 
 # replace php.ini and fpm vars
-sed -i "s/memory_limit = 128M/memory_limit = 512M/"  /etc/php/7.4/fpm/php.ini
-sed -i "s/listen = 9000/listen = \/run\/php\/php7.4-fpm.sock/"  /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/memory_limit = .*.M/memory_limit = 1024M/g"  /etc/php/7.4/fpm/php.ini
+sed -i "s/listen = .*/listen = \/run\/php\/php7.4-fpm.sock/g"  /etc/php/7.4/fpm/pool.d/www.conf
 
 # to do: make prod  
 cd /var/www/html && NO_DOCKER=true make prod 
